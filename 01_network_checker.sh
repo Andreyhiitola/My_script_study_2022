0600d84be5172eed087b86fef28b67dbf0a3742f
Andreyhiitola <9624294@gmail.com>
2022-01-31 17:36:29 +0300

    Написание скриптов баш и обучение работы с git

---
+++ 01_network_checker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#LOG=/tmp/mylog.log
+# name1 задает адрес google.com
+name1=google.com
+# name2 задает адрес gateway хоста
+ 
+ name2=127.0.0.1
+
+
+
+       	 ping -c 5 -i 2 "$name1" > logfile.txt 
+
+
+if [ "$?" == 0  ];
+then 
+ echo ""$name1" reachable"
+else 
+echo "ERROR: cannot reach the "$name1"" 	
+fi
+
+            ping -c 5 -i 2 "$name2" >> logfile.txt
+
+if [ "$?" == 0 ];
+then 
+	echo "The default gateway   "$name2" reacheble"  
+else 
+
+	echo "ERROR: The default gateway is not reacheble" | tee logfile.txt 
+fi
+	
+exit 1
+
+
+
+
---
+++ backup_script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Скрипт для backup с использованием rsync  cron
+# В планировщике cron описываем время, дату запуска скрипта backup_script
+# nano /etc/crontab 
+
+source=/home/andy/Desktop/source_1/               #путь к папке с source
+
+distination=/home/andy/Desktop/distination.back/  #путь к папке distination.back
+
+#/home/andy/Desktop/log_file/log-file.txt/        #папка записи логов
+
+rsync -avz --delete $source $distination > /home/andy/Desktop/log_file/log-file.txt`date +%F-%H-%M`
+
+
+               
+
+
+
+
+
+ 
+#echo "$source"
+
+#echo "$distination"
