b61ab0a2db1dff4fca28bea79c2929ebd00f1a58
Andreyhiitola <9624294@gmail.com>
2022-02-06 23:04:26 +0300

    изменения внесенные 06.02.22

--- 01_network_checker.sh
+++ 01_network_checker.sh
@@ -1,34 +1,26 @@
-#!/bin/bash
-#LOG=/tmp/mylog.log
-# name1 задает адрес google.com
-name1=google.com
-# name2 задает адрес gateway хоста
- 
- name2=127.0.0.1
+#!/usr/bin/env bash 
 
 
-
-       	 ping -c 5 -i 2 "$name1" > logfile.txt 
-
-
-if [ "$?" == 0  ];
-then 
- echo ""$name1" reachable"
-else 
-echo "ERROR: cannot reach the "$name1"" 	
-fi
-
-            ping -c 5 -i 2 "$name2" >> logfile.txt
-
-if [ "$?" == 0 ];
-then 
-	echo "The default gateway   "$name2" reacheble"  
-else 
-
-	echo "ERROR: The default gateway is not reacheble" | tee logfile.txt 
+  # name1 задает адрес google.com
+  name1=google.com
+  #  через ip r находим default gateway
+    ping -c 5 -i 2 "$name1" > logfile.txt 
+  if [ "$?" == 0  ];
+  then 
+  echo ""$name1" reachable"
+  else 
+  echo "ERROR: cannot reach the $name1" 	
+  fi
+  ping -c 5 -i 2  $(ip r | grep default | awk '{print$3}') >> logfile.txt
+  if [ "$?" == 0 ];
+  then 
+  echo "The default gateway reacheble"  
+  else 
+  echo "ERROR: The default gateway is not reacheble" | tee logfile.txt 
 fi
 	
-exit 1
+
+
 
 
 
